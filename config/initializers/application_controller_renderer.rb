2aa0c613166e6a26f96c8975dfcdd5446914eb6e
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-03-06 10:33:44 +0000

    [Add] 6-3 投稿一覧画面

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3b0924ebd8851c08d82aeb006761de5a52c9886ad9521348c00ba699c782ddc73b38eb23a07b2536e19eda409d42480a04d01fcc066e531d5b9ed3434ecdca16'
